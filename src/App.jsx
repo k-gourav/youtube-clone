2772279b6a248d13082e85baa45c1cc348238d76
k-gourav <code2change.gourav@gmail.com>
2024-06-22 13:15:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Shorts from "./components/Shorts/Shorts";
+
+
+const App = () => {
+    return (
+        <>
+        <Routes>
+           <Route path="/" element={ <Home /> } />
+           <Route path="shorts" element={ <Shorts /> } />
+        </Routes>
+        </>
+    )
+}
+
+export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>Hello how are you?</div>
+    );
+}
+
+export default Home;
